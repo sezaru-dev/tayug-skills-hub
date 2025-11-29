1a913f8585ed545fcb6b593e6fd2b2bd01c07662
sezaru-dev <cezarbaguhin20@gmail.com>
2025-11-29 22:14:05 +0800

    feat: create a Footer section component

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white">
+      <div className="max-w-7xl mx-auto text-center py-6 ">
+        &copy; 2025 Tayug Skills Hub. All rights reserved.
+      </div>
+    </footer>
+  );
+}
