f13f4b28f96d2defaf08b0028f9717cb6e82f2f9
sezaru-dev <cezarbaguhin20@gmail.com>
2025-11-30 22:20:00 +0800

    feat(ui): add shadcn sign-up-form component

---
+++ src/components/sign-up-form.tsx
@@ -0,0 +1,66 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+
+export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          Enter your information below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <Input id="password" type="password" required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="confirm-password">
+                Confirm Password
+              </FieldLabel>
+              <Input id="confirm-password" type="password" required />
+            </Field>
+            <FieldGroup>
+              <Field>
+                <Button type="submit" className="primary-button">Create Account</Button>
+                <Button variant="outline" type="button">
+                  Sign up with Github
+                </Button>
+                <FieldDescription className="px-6 text-center">
+                  Already have an account? <Link href="/auth/login">Sign in</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
