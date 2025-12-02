d87dc12a663cf15b035dadb6bed6b57d8147b135
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-02 21:58:29 +0800

    feat(section): add WhatMakesUsDifferent section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import SearchBar from "@/components/custom/SearchBar";
 import CTASection from "@/components/sections/CTA";
 import Footer from "@/components/sections/Footer";
 import Hero from "@/components/sections/Hero";
 import HowThePlatformWorks from "@/components/sections/HowThePlatformWorks";
+import WhatMakesUsDifferent from "@/components/sections/WhatMakesUsDifferent";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
       <HowThePlatformWorks/>
+      <WhatMakesUsDifferent/>
       <CTASection/>
       <Footer/>
     </div>
