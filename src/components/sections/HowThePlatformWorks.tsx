f0e6d9c9e2cf5792c7c8512944701fb98bc066a6
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-02 21:33:02 +0800

    refactor(component): update client and freelancer steps

--- src/components/sections/HowThePlatformWorks.tsx
+++ src/components/sections/HowThePlatformWorks.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {ArrowRightLeft} from 'lucide-react'
+import { clientSteps, freelancerSteps } from '@/app/constant-data'
 
 export default function HowThePlatformWorks() {
   return (
     <section className='px-4 py-24 bg-gray-100'>
       <div className='max-w-7xl mx-auto'>
-        <header className='mb-12 text-center max-w-2xl mx-auto space-y-4'>
+        <header className='mb-12 text-center max-w-3xl mx-auto space-y-4'>
           <h2 className='text-4xl font-medium text-foreground'>How The Platform Works</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorem aut fugit sapiente soluta, in distinctio at quisquam amet, aperiam, officiis natus nulla!</p>
+          <p>Our streamlined process connects Tayug clients with skilled professionals through a simple yet powerful system that ensures trust, convenience, and satisfaction for both sides.</p>
         </header>
         <div className='max-w-5xl mx-auto grid grid-cols-11 grid-rows-1'>
 
@@ -16,86 +17,42 @@ export default function HowThePlatformWorks() {
               <h4 className='uppercase font-semibold text-blue-600'>for clients</h4>
             </header>
             <ol className='space-y-4'>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-600 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>1</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-600 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>2</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-600 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>2</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-600 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>2</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
+              {
+                clientSteps.map((step) => (
+                  <li className='flex' key={step.id}>
+                    <div className='mr-2'>
+                      <div className='col-span-1 font-bold bg-blue-600 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>{step.id}</div>
+                    </div>
+                    <div className='col-span-11'>
+                      <h5 className='font-semibold'>{step.title}</h5>
+                      <p className='text-gray-600'>{step.description}</p>
+                    </div>
+                  </li>
+                ))
+              }
             </ol>
           </div>
 
-          <div className='p-6 border col-span-5 col-start-7 col-end-12 relative bg-white rounded-md'>
+          <div className='p-6 border col-span-5 col-start-7 col-end-12 relative bg-white rounded-md z-20'>
             <header className='mb-4'>
               <h4 className='uppercase font-semibold text-blue-950'>for freelancers</h4>
             </header>
             <ol className='space-y-4'>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-950 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>1</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-950 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>2</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-950 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>2</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
-              <li className='flex'>
-                <div className='mr-2'>
-                  <div className='col-span-1 font-bold bg-blue-950 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>2</div>
-                </div>
-                <div className='col-span-11'>
-                  <h5 className='font-semibold'>Create an Account</h5>
-                  <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique?</p>
-                </div>
-              </li>
+              {
+                freelancerSteps.map( ({id, title, description}) => (
+
+                <li className='flex' key={id}>
+                  <div className='mr-2'>
+                    <div className='col-span-1 font-bold bg-blue-950 text-white rounded-full h-8 w-8 grid place-items-center mr-6'>{id}</div>
+                  </div>
+                  <div className='col-span-11'>
+                    <h5 className='font-semibold'>{title}</h5>
+                    <p className='text-gray-600'>{description}</p>
+                  </div>
+                </li>
+                ))
+              }
+
             </ol>
           </div>
           <div className='col-span-1 row-start-1 col-start-6 col-end-7 h-full grid place-items-center relative'>
