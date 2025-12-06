91b10eddc7b1caa33dee8322a2ae223123bea8f4
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-06 22:38:53 +0800

    chore(layout): temporarily comment out Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body
         className={` ${poppins.variable} antialiased`}
       >
-        <Header/>
+        {/* <Header/> */}
         {children}
       </body>
     </html>
