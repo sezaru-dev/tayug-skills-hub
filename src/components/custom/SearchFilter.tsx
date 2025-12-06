5ac4ef5f2dca6e9400bee73f38e92ade97d60795
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-06 22:28:33 +0800

    feat: add reusable SearchFilter component

---
+++ src/components/custom/SearchFilter.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Input } from '../ui/input'
+
+type SearchFilterProps = {
+  value?: string
+  onChange?: (event: string) => void
+  cName?: string
+  placeholder?: string
+}
+
+export default function SearchFilter({value, onChange, cName, placeholder}: SearchFilterProps) {
+  return (
+    <Input
+      placeholder={`${placeholder ?? "Search name or service..." }`}
+      value={value}
+      /* onChange={(event) => onChange(event.target.value) } */
+      className={`max-w-sm ${cName}`}
+    />
+  )
+}
