d15796f62e178b26e8c110acb2d124f5781fb7e4
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-12 22:12:32 +0800

    refactor(page-dashboard): comment ServiceProviderCard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -147,7 +147,7 @@ export default function Page() {
 
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {providers.map((provider) => (
+{/*           {providers.map((provider) => (
           <ServiceProviderCard
             key={provider.id}
             id={provider.id}
@@ -156,7 +156,7 @@ export default function Page() {
             skills={provider.skills}
             location={provider.location}
           />
-        ))}
+        ))} */}
         </div>
       </main>
       </SidebarInset>
