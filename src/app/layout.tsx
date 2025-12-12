3df1032b0c8e82e585e39c1f587de7dc60599a7e
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-12 22:07:39 +0800

    feat(layout): add authjs SessionProvider and enclose the children components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from 'next/font/google';
 import "./globals.css";
+import { SessionProvider } from "next-auth/react"
 /* import Header from "@/components/sections/Header"; */
 
 const poppins = Poppins({
@@ -26,7 +27,9 @@ export default function RootLayout({
         className={` ${poppins.variable} antialiased`}
       >
         {/* <Header/> */}
-        {children}
+        <SessionProvider>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
