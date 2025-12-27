d0eac3c917abb72ed759e83fc21831f50a229268
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-27 12:50:27 +0800

    refactor(auth): use NextAuth with authOptions and set runtime to Node.js

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,10 @@
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
+export const runtime = "nodejs";
+
+import { authOptions } from "@/lib/auth"
+import NextAuth from "next-auth"
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
+
+
