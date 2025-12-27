b072dc99d7dde6fa55fc79f32d193c9b3300f596
sezaru-dev <cezarbaguhin20@gmail.com>
2025-12-27 10:52:33 +0800

    chore(discover): move saved-providers files into Discover folder

---
+++ src/app/dashboard/(discover)/saved-providers/page.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Page() {
+  
+  return (
+    <main className="flex-1 p-6 md:p-8 space-y-6 mt-8">
+      <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-4">
+        <div>
+          <h1 className="text-2xl font-bold tracking-tight">List of bookmarked providers</h1>
+          <p className="text-muted-foreground text-sm">
+            Access the service providers you’ve saved for quick reference and easy connection.
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center gap-4 py-4">
+        {/*  */}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      </div>
+    </main>
+  )
+}
