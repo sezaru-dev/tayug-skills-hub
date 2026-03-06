ee7959574ac131d2b254a0c34d9f151931434efb
sezaru-dev <cezarbaguhin20@gmail.com>
2026-03-06 21:40:24 +0800

    feat(api): create delete category api, check if the category name and confirmation text matches then proceed to deletion

--- src/app/api/categories/[id]/route.ts
+++ src/app/api/categories/[id]/route.ts
@@ -42,9 +42,51 @@ export async function PATCH (req: Request, { params }: { params: { id: string } 
 
     return NextResponse.json(updatedCategoryName, { status: 200 });
   } catch (error) {
-    console.error(error);
-    
+
     let message = "Failed to rename category";
+    if (error instanceof Error) {
+      message = error.message;
+    }
+    return NextResponse.json({ error: message }, { status: 500 });  
+  }
+}
+
+//This route will handle deleting a category. It will receive the category ID as a parameter and a confirmation text in the request body. It will check if the confirmation text matches the category name before proceeding with the deletion. It will also verify the session to ensure that only admins can delete categories. 
+export async function DELETE (req: Request, { params }: { params: { id: string } }) {
+  
+  //  Verify session
+  const sessionOrResponse = await verifySession([Role.ADMIN])
+  
+  if (sessionOrResponse instanceof NextResponse) {
+    return sessionOrResponse // EARLY RETURN
+  }
+  
+  
+  try {
+    const { id } = params;
+
+    const body = await req.json()
+    const { confirmText } = body
+
+    if (!id || !confirmText) {
+      return NextResponse.json({ error: "Missing data" }, { status: 400 })
+    }
+
+    const isNameMatch = await CategoryRepository.crossmatchCategoryName(id, confirmText)
+    if (!isNameMatch) {
+      return NextResponse.json(
+        { error: "Category name does not match." },
+        { status: 409 }
+      );
+    }
+
+    // Proceed with deleting the category
+    const deleteCategory = await CategoryRepository.deleteCategory(id)
+    return NextResponse.json(deleteCategory, { status: 200 });
+
+  } catch (error) {
+    
+    let message = "Failed to delete category";
     if (error instanceof Error) {
       message = error.message;
     }
