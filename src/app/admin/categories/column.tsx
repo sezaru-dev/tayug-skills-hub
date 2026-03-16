c67cc8a9d30d13ef7408a3ddff70c3115fc7952a
sezaru-dev <cezarbaguhin20@gmail.com>
2026-03-16 22:45:43 +0800

    refactor(category-column): refactor column and add skillcounttooltip, remove createdAt, refactor viewskilldialog and and conditional rendering on PermanentDeleteAlertDialog

--- src/app/admin/categories/column.tsx
+++ src/app/admin/categories/column.tsx
@@ -12,11 +12,12 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
 import { MoreHorizontal } from "lucide-react"
-import ToggleStatusDropdownMenuItem from "@/features/admin/categories/components/toggle-status-dropdownmenuitem"
 import RenameCategoryDialog from "@/features/admin/categories/components/rename-category-dialog"
 import { PermanentDeleteAlertDialog } from "@/features/admin/categories/components/permanent-delete-alert-dialog"
 import ViewSkillsDialog from "@/features/admin/categories/components/view-skills-dialog"
 import { Category } from "@/features/admin/categories/types"
+import { ChangeCategoryStatusAlertDialog } from "@/features/admin/categories/components/change-status/ChangeCategoryStatusAlertDialog"
+import SkillCountTooltip from "@/features/admin/categories/components/SkillCountTooltip"
 
 export const columns: ColumnDef<Category>[] = [
   {
@@ -38,14 +39,14 @@ export const columns: ColumnDef<Category>[] = [
     }
   },
   {
-    accessorKey: "skills",
+    id: "skills",
     header: "Skills",
-    cell: ({ row }) => {
-      const skills = row.getValue("skills") as string[];
-      if (!skills || skills.length === 0) {
-        return <span className="text-gray-500">0</span>;
-      }
-      return  (skills.length);
+    accessorFn: (row: Category) => row.skills ?? [], // always return array
+    cell: ({ getValue }) => {
+      const skills = getValue<Category["skills"]>() ?? [] // fallback to empty array
+      return skills.length === 0
+        ? <span className="text-gray-500">0</span>
+        : <SkillCountTooltip skills={skills}><span className="cursor-pointer">{skills.length}</span></SkillCountTooltip>
     },
   },
   {
@@ -81,32 +82,20 @@ export const columns: ColumnDef<Category>[] = [
       )
     },
   },
-
-  {
-    accessorKey: "createdAt",
-    header: "Created At",
-    cell: ({ row }) => {
-      const date = new Date(row.getValue("createdAt") as string);
-      return (
-        <time dateTime={date.toISOString()} className="text-nowrap">
-          {`${date.toLocaleDateString("en-PH", {
-            day: "2-digit",
-            month: "short",
-            year: "numeric",
-            timeZone: "Asia/Manila",
-          })} · ${date.toLocaleTimeString("en-PH", {
-            hour: "numeric",
-            minute: "2-digit",
-            hour12: true,
-            timeZone: "Asia/Manila",
-          })}`}
-        </time>
-      );
-    },
-  },
   {
     accessorKey: "updatedAt",
-    header: "Updated At",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="pl-0"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          UpdatedAt
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
     cell: ({ row }) => {
       const date = new Date(row.getValue("updatedAt") as string);
       return (
@@ -132,7 +121,6 @@ export const columns: ColumnDef<Category>[] = [
     header: "Actions",
     cell: ({ row }) => {
       const id = row.original.id
-      const skills = row.getValue("skills") as string[];
       const isActive = row.getValue("isActive") as boolean;
 
       return (
@@ -146,18 +134,22 @@ export const columns: ColumnDef<Category>[] = [
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
 
-
-            <ViewSkillsDialog/>
+            {/* view skills action */}
+            <ViewSkillsDialog category={row.original.name} skills={row.original.skills} />
 
             <DropdownMenuSeparator />
 
             {/* rename category action */}
             <RenameCategoryDialog id={row.original.id} currentName={row.original.name} />
 
+            {/* change category status action */}
+            <ChangeCategoryStatusAlertDialog id={id} isActive={isActive} />
 
-            <ToggleStatusDropdownMenuItem categoryId={id} isActive={isActive} />
+            {/* permanent delete category action */}
+            {row.original.skills?.length === 0 && (
+              <PermanentDeleteAlertDialog id={row.original.id} categoryName={row.original.name} />
+            )}
 
-            <PermanentDeleteAlertDialog id={row.original.id} categoryName={row.original.name} />
           </DropdownMenuContent>
         </DropdownMenu>
       )
