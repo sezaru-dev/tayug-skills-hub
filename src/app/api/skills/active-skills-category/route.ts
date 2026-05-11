b57950145c9f0ecc8da6b07c6e20da65f805f223
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-11 20:30:00 +0800

    feat(api): add endpoint to get active skills

---
+++ src/app/api/skills/active-skills-category/route.ts
@@ -0,0 +1,26 @@
+import { SkillRepository } from "@/features/skills/skill-repository";
+import { verifySession } from "@/lib/verify-session";
+import { Role } from "@/types/roles";
+import { NextResponse } from "next/server";
+
+export async function GET (req: Request){
+  //  Verify session
+  const sessionOrResponse = await verifySession([Role.ADMIN, Role.USER, Role.PROVIDER])
+
+  if (sessionOrResponse instanceof NextResponse) {
+    return sessionOrResponse // EARLY RETURN
+  }
+
+  try {
+
+      const skills = await SkillRepository.getActiveSkills();
+      return NextResponse.json(skills, { status: 200 })
+  
+    } catch (error) {
+      console.error("[SKILL_GET]", error)
+      return NextResponse.json(
+        { message: "Internal server error" },
+        { status: 500 }
+      )
+    }
+}
