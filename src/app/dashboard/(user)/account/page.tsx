9fe1ddb0a05790a8ad3bdfa6e632d015bbe7a968
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-11 20:48:22 +0800

    refactor(page): refactor service provider account settings page

--- src/app/dashboard/(user)/account/page.tsx
+++ src/app/dashboard/(user)/account/page.tsx
@@ -1,22 +1,39 @@
+import LoadingSkeleton from "@/components/custom/LoadingSkeleton"
+import AccountDetailsSection from "@/features/account/components/account-details/AccountDetailsSection"
+import ChangePasswordSection from "@/features/account/components/change-password/ChangePasswordSection"
+import { DeleteAccountSection } from "@/features/account/components/delete-account/DeleteAccountSection"
+import { verifySession } from "@/lib/verify-session"
+import { Role } from "@/types/roles"
+import { NextResponse } from "next/server"
+import { Suspense } from "react"
 
-export default function Page() {
-
+export default async function AccountSettingsPage() {
+  const session = await verifySession([Role.PROVIDER])
+  
+  if (session instanceof NextResponse) {
+    return session
+  }
+  
+  const id = session.user.id
   return (
-    <main className="flex-1 p-6 md:p-8 space-y-6 mt-8">
-      <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-4">
-        <div>
-          <h1 className="text-2xl font-bold tracking-tight">Account Settings</h1>
-          <p className="text-muted-foreground text-sm">
-            Manage your profile, update personal information, and customize your account preferences.
-          </p>
-        </div>
-      </div>
-      <div className="flex items-center gap-4 py-4">
-      {/*  */}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {/*  */}
-      </div>
+    <main className="flex-1 p-6 md:p-8 space-y-6 w-full max-w-2xl mx-auto">
+
+      {/* header */}
+      <header className="mb-10 space-y-2">
+        <h1 className="text-3xl font-semibold tracking-tight">Account Settings</h1>
+        <p className="text-sm text-muted-foreground max-w-2xl">
+          Update account details, change password, and account deletion.
+        </p>
+      </header>
+
+      {/* Profile */}
+      <Suspense fallback={<LoadingSkeleton />}>
+        <AccountDetailsSection id={id}/>
+      </Suspense>
+
+      <ChangePasswordSection/>
+      <DeleteAccountSection/>         
+
     </main>
   )
 }
