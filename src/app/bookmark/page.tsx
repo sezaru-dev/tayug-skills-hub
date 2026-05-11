251d47d176e6a8b3475d335ce463d84087b9fce0
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-11 20:59:40 +0800

    feat(page): add public bookmark page

---
+++ src/app/bookmark/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BrowseProvidersPage = () => {
+  return (
+    <main className='max-w-7xl mx-auto flex justify-between items-center p-4'>
+      <header>
+        <h1 className='text-2xl'>List of bookmarked providers</h1>
+        <p>Access the service providers you’ve saved for quick reference and easy connection.</p>
+      </header>
+    </main>
+  )
+}
+
+export default BrowseProvidersPage
