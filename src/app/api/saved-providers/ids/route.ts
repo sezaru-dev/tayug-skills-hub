a874b3f07bc757703ad381ad15b2b126aab9e182
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-11 20:32:34 +0800

    feat(api): add GET endpoint to get IDs of the saved providers

---
+++ src/app/api/saved-providers/ids/route.ts
@@ -0,0 +1,27 @@
+import { SavedProviderRepository } from "@/features/saved-provider/saved-provider-repository";
+import { verifySession } from "@/lib/verify-session";
+import { Role } from "@/types/roles";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const session = await verifySession([Role.PROVIDER])
+
+    if (session instanceof NextResponse) {
+      return session
+    }
+
+    const userId = session.user.id
+
+    const savedProviders =
+    await SavedProviderRepository.getSavedIds(userId)
+
+    return NextResponse.json(savedProviders, { status: 200 })
+  } catch (err) {
+    console.error(err)
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    )
+  }
+}
