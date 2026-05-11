d93493f76b856cb060b27e2eb02337400ee80dc6
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-11 20:37:43 +0800

    refactor(ui): refactor signup form ui

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -86,96 +86,95 @@ export function SignupForm({
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-            <CardTitle>Create an account</CardTitle>
-            <CardDescription>Enter your information below to create your account</CardDescription>
-        </CardHeader>
+      
+      <div className="flex flex-col items-center gap-1 text-center">
+        <h1 className="text-2xl font-bold">Create an account</h1>
+        <p className="text-sm text-balance text-muted-foreground">
+          Enter your information below to create your account
+        </p>
+      </div>
 
-        <CardContent>
-          <form id="signup-form" onSubmit={form.handleSubmit(onSubmit)}>
-            <FieldGroup>
-              <Controller
-                name="name"
-                control={form.control}
-                render={({field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-input-name">
-                      Name
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-input-name"
-                      aria-invalid={fieldState.invalid}
-                      autoComplete="name"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]}/>
-                    )}
-                  </Field>
+      <form id="signup-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <FieldGroup>
+          <Controller
+            name="name"
+            control={form.control}
+            render={({field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="form-input-name">
+                  Name
+                </FieldLabel>
+                <Input
+                  {...field}
+                  id="form-input-name"
+                  aria-invalid={fieldState.invalid}
+                  autoComplete="name"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]}/>
                 )}
-              />
-              <Controller
-                name="email"
-                control={form.control}
-                render={({field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-input-email">
-                      Email
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-input-email"
-                      aria-invalid={fieldState.invalid}
-                      autoComplete="email"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]}/>
-                    )}
-                  </Field>
-                )}
-              />
-              <Controller
-                name="password"
-                control={form.control}
-                render={({field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-input-password">
-                      Password
-                    </FieldLabel>
-                    <div className="relative">
-                      <Input
-                        {...field}
-                        type={showPassword ? "text" : "password"}
-                        id="form-input-password"
-                        aria-invalid={fieldState.invalid}
-                        autoComplete="email"
-                      />
-                      <ShowHidePasswordButton show={showPassword} onToggle={() => setShowPassword(prev => !prev)}/>
-                    </div>
-                    <FieldDescription className="text-xs">
-                      At least 8 chars, with uppercase, lowercase, number & symbol.
-                    </FieldDescription>
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]}/>
-                    )}
-                  </Field>
-                )}
-              />
-              <Field>
-                <Button type="submit" className="bg-blue-600 hover:bg-blue-700" form="signup-form">Create Account</Button>
-{/*                 <Button variant="outline" type="button" onClick={() => signIn("google", {callbackUrl: "/dashboard"})}>
-                  Sign up with Github
-                </Button> */}
               </Field>
-                <FieldDescription className="text-center">
-                  Already have an account? <Link href="/auth/login">Sign in</Link>
+            )}
+          />
+          <Controller
+            name="email"
+            control={form.control}
+            render={({field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="form-input-email">
+                  Email
+                </FieldLabel>
+                <Input
+                  {...field}
+                  id="form-input-email"
+                  aria-invalid={fieldState.invalid}
+                  autoComplete="email"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]}/>
+                )}
+              </Field>
+            )}
+          />
+          <Controller
+            name="password"
+            control={form.control}
+            render={({field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="form-input-password">
+                  Password
+                </FieldLabel>
+                <div className="relative">
+                  <Input
+                    {...field}
+                    type={showPassword ? "text" : "password"}
+                    id="form-input-password"
+                    aria-invalid={fieldState.invalid}
+                    autoComplete="email"
+                  />
+                  <ShowHidePasswordButton show={showPassword} onToggle={() => setShowPassword(prev => !prev)}/>
+                </div>
+                <FieldDescription className="text-xs">
+                  At least 8 chars, with uppercase, lowercase, number & symbol.
                 </FieldDescription>
-            </FieldGroup>
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]}/>
+                )}
+              </Field>
+            )}
+          />
+          <Field>
+            <Button type="submit" className="bg-blue-600 hover:bg-blue-700" form="signup-form">Create Account</Button>
+{/*                 <Button variant="outline" type="button" onClick={() => signIn("google", {callbackUrl: "/dashboard"})}>
+              Sign up with Github
+            </Button> */}
+          </Field>
+            <FieldDescription className="text-center">
+              Already have an account? <Link href="/auth/login">Sign in</Link>
+            </FieldDescription>
+        </FieldGroup>
 
-          </form>
-        </CardContent>
-      </Card>
+      </form>
 
     </div>
   )
