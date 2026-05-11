9edf5b17c14fb8d2203638224bfdccef53286bfa
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-11 20:16:46 +0800

    feat(api): add endpoint to get service provider data for admin

---
+++ src/app/api/admin/service-providers/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server"
+import { verifySession } from "@/lib/verify-session"
+import { Role } from "@/types/roles"
+import { ProviderDiscoveryRepository } from "@/features/provider-discovery/provider-discovery.repository"
+
+export async function GET(_req: Request) {
+  try {
+    //  Verify session
+      const session = await verifySession([Role.ADMIN])
+    
+      if (session instanceof NextResponse) {
+        return session
+      }
+      const userId = session.user.id
+
+    const users = await ProviderDiscoveryRepository.getAdminProviders()
+
+    return NextResponse.json(users, { status: 200 })
+  } catch (error) {
+    console.error("GET /api/admin/service-providers error:", error)
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to fetch providers",
+      },
+      { status: 500 }
+    )
+  }
+}
