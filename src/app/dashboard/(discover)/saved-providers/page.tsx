e6e912a519b560d132b44fab88355e7cc4bfd77b
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-11 20:47:11 +0800

    refactor(page): refactor saved provider page

--- src/app/dashboard/(discover)/saved-providers/page.tsx
+++ src/app/dashboard/(discover)/saved-providers/page.tsx
@@ -1,22 +1,38 @@
+import SavedProvidersList from "@/features/saved-provider/components/SavedProviderList"
+import { SavedProvidersListSkeleton } from "@/features/saved-provider/components/SavedProvidersListSkeleton"
+import { verifySession } from "@/lib/verify-session"
+import { Role } from "@/types/roles"
+import { NextResponse } from "next/server"
+import { Suspense } from "react"
 
+export default async function Page() {
+  const session = await verifySession([Role.PROVIDER])
 
-export default function Page() {
+  if (session instanceof NextResponse) {
+    return session
+  }
+  
+  const userId = session.user.id
+
   
   return (
-    <main className="flex-1 p-6 md:p-8 space-y-6 mt-8">
-      <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-4">
-        <div>
-          <h1 className="text-2xl font-bold tracking-tight">List of bookmarked providers</h1>
-          <p className="text-muted-foreground text-sm">
-            Access the service providers you’ve saved for quick reference and easy connection.
-          </p>
-        </div>
-      </div>
-      <div className="flex items-center gap-4 py-4">
-        {/*  */}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-      </div>
+    <main className="flex-1 p-6 md:p-8 space-y-2 md:space-y-3">
+      <header className="mb-10 space-y-2">
+        <h1 className="text-2xl sm:text-3xl font-semibold tracking-tight leading-tight">
+          List of bookmarked providers
+        </h1>
+
+        <p className="text-xs sm:text-sm text-muted-foreground max-w-full sm:max-w-xl md:max-w-2xl leading-relaxed">
+          Access the service providers you’ve saved for quick reference and easy connection.
+        </p>
+      </header>
+
+      <Suspense fallback={<SavedProvidersListSkeleton />}>
+        <SavedProvidersList userId={userId} />
+      </Suspense>
+
+
+      
     </main>
   )
 }
