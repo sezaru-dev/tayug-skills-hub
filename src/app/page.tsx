7cac2b6f83bba5b7f9eb8ae1c38f4ed2b8021c52
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-12 23:50:07 +0800

    refactor(page): refactor homepage to have server side data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import CTASection from "@/components/sections/CTA";
-import Footer from "@/components/sections/Footer";
 import Hero from "@/components/sections/Hero";
 import HowThePlatformWorks from "@/components/sections/HowThePlatformWorks";
 import WhatMakesUsDifferent from "@/components/sections/WhatMakesUsDifferent";
+import { SkillRepository } from "@/features/skills/skill-repository";
 
-export default function Home() {
+export default async function Home() {
+  const activeSkills = await SkillRepository.getActiveSkillsNameId()
+
   return (
     <div>
-      <Hero/>
+      <Hero activeSkills={activeSkills}/>
       <HowThePlatformWorks/>
       <WhatMakesUsDifferent/>
       <CTASection/>
