7b5cf02ca3f451af0b7f747931461a8f07088647
sezaru-dev <cezarbaguhin20@gmail.com>
2026-05-14 14:05:35 +0800

    chore(page): remove commented code

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,3 @@
-/* import { LoginForm } from "@/components/login-form";
-
-export default function LoginPage() {
-  return (
-    <div className="bg-background flex min-h-[calc(100svh-5rem)] flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
-  )
-} */
-
 import { LoginForm } from "@/components/login-form"
 import { SquareLibrary } from "lucide-react"
 import Link from "next/link"
